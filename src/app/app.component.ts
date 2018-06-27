33945428bf39eff41dff548824af5b407cd69df4
Riyaz ahmed <riyazahm3d@gmail.com>
2018-06-27 11:08:41 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  review = '';
+  data: any;
+  res: any;
+  intent: Object;
+  primary: Object;
+  secondary: Object[];
+
+constructor(private http: HttpClient) { }
+
+   onClickMe(data) {
+    this.review = data;
+    this.http.post('http://192.168.1.79:3000/pages/home', {comment: this.review}).subscribe(res => {
+      this.intent = res.intent;
+      this.primary = res.primary;
+      this.secondary = res.secondary[0];
+      console.log(res.secondary[0].length);
+      console.log(this.secondary.length);
+    });
+  }
 }
